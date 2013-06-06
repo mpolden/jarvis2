bc51b92dd940b01458b770fffc3cc2cb04a31d2f
Martin Polden <martin.polden@gmail.com>
2013-06-06 10:59:12 +0200

    Configure Angular app

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+/* Setup Angular app */
+var jarvis = angular.module('jarvis', []);
+
+jarvis.controller('EventCtrl', ['$scope', function ($scope) {
+
+  var source = new EventSource('/events');
+
+  source.addEventListener('message', function (message) {
+    $scope.$apply(function () {
+      $scope.data = JSON.parse(message.data);
+    });
+  }, false);
+
+}]);
+
+/* Initialize gridster */
+$(function () {
+  $(".gridster ul").gridster({
+    widget_margins: [10, 10],
+    widget_base_dimensions: [140, 140]
+  });
+});
