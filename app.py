1d391ef70edcb81a7a6f27685e2dca103b24bd66
Martin Polden <martin.polden@gmail.com>
2013-06-06 10:56:02 +0200

    Implement server-sent events and basic worker support

--- app.py
+++ app.py
@@ -2,9 +2,15 @@
 
 import sys
 import os
-from flask import Flask, render_template, make_response
+import yaml
+import inspect
+from flask import Flask, render_template, make_response, Response, \
+    stream_with_context
+
+import workers
 
 app = Flask(__name__)
+active_workers = []
 
 
 @app.route('/')
@@ -21,8 +27,34 @@ def css():
     return response
 
 
+@app.route('/events')
+def events():
+    def generate():
+        for worker in active_workers:
+            yield 'data: %s\n\n' % (worker.get(),)
+    return Response(stream_with_context(generate()),
+                    mimetype='text/event-stream')
+
+
+def _read_conf():
+    with open(os.path.join(sys.path[0], 'config.yml'), 'r') as conf:
+        return yaml.load(conf)
+
+
+def _configure_jobs(conf):
+    for cls_name, cls in inspect.getmembers(workers, inspect.isclass):
+        name = cls_name.lower()
+        if name not in conf.keys() or not conf[name]['enabled']:
+            print 'Skipping missing or disabled worker: %s' % (name,)
+            continue
+        print 'Configuring worker: %s' % (name,)
+        worker = cls(conf)
+        active_workers.append(worker)
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'debug':
         app.debug = True
+    _configure_jobs(_read_conf())
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=True)
