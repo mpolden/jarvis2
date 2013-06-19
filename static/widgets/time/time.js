2ed50ebe607775dd75966fe9980477cf83f83069
Martin Polden <martin.polden@gmail.com>
2013-06-19 14:51:04 +0200

    Add time widget

---
+++ static/widgets/time/time.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var jarvis = jarvis || angular.module('jarvis', []);
+
+jarvis.controller('TimeCtrl', ['$scope', function ($scope) {
+
+  var fmtTime = function (n) {
+    return n < 10 ? '0' + n : n;
+  };
+
+  var getDayName = function (date) {
+    return ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag',
+            'lørdag'][date.getDay()];
+  };
+
+  var getMonthName = function (date) {
+    return ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli',
+            'august', 'september', 'oktober', 'november',
+            'desember'][date.getMonth()];
+  };
+
+  var setTime = function () {
+    var today = new Date(),
+        h = fmtTime(today.getHours()),
+        m = fmtTime(today.getMinutes()),
+        y = today.getFullYear();
+
+    $scope.$apply(function () {
+      $scope.time = h + ':' + m;
+      $scope.date = today.getDate() + '. ' + getMonthName(today) + ' ' + y;
+      $scope.day = getDayName(today);
+    });
+  };
+
+  setInterval(setTime, 500);
+ 
+}]);
+
