4cd1d452ed7cbb8e9d1ec1b2c5f2348a78f2ab0b
Martin Polden <martin.polden@gmail.com>
2013-06-30 21:28:24 +0200

    Implement Plex widget

---
+++ app/static/widgets/plex/plex.js
@@ -0,0 +1,18 @@
+var jarvis = jarvis || angular.module('jarvis', []);
+
+jarvis.controller('PlexCtrl', ['$scope',
+  function ($scope) {
+    'use strict';
+
+    $scope.$on('plex', function (ev, body) {
+      if (body.movies.length > 5) {
+        body.movies = body.movies.slice(0, 5);
+      }
+      if (body.shows.length > 5) {
+        body.shows = body.shows.slice(0, 5);
+      }
+      $scope[ev.name] = body;
+    });
+
+  }
+]);
