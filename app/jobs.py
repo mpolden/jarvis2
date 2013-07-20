ae29715a9fc6dcf560952e5a8206d32cb494a5ed
Martin Polden <martin.polden@gmail.com>
2013-07-20 03:01:34 +0200

    Do not reuse IMAP4 instance in Gmail job

--- app/jobs.py
+++ app/jobs.py
@@ -354,7 +354,6 @@ class Gmail(AbstractJob):
         self.email = conf['email']
         self.password = conf['password']
         self.folder = conf['folder']
-        self.mail = imaplib.IMAP4_SSL('imap.gmail.com', 993)
 
     def _parse_count(self, message):
         count = re.search('\w+ (\d+)', message)
@@ -369,13 +368,16 @@ class Gmail(AbstractJob):
         return self._parse_count(message[0])
 
     def get(self):
-        if self.mail.state == 'NONAUTH':
-            self.mail.login(self.email, self.password)
+        self.mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        self.mail.login(self.email, self.password)
+        count = self._get_count()
+        unread_count = self._get_unread_count()
+        self.mail.logout()
         return {
             'email': self.email,
             'folder': self.folder,
-            'count': self._get_count(),
-            'unread_count': self._get_unread_count()
+            'count': count,
+            'unread_count': unread_count
         }
 
 
