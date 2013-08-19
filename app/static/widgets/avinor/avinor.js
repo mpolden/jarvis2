b1dee13cc512a76317cd803ae2c8889ddf1afea9
Martin Polden <martin.polden@gmail.com>
2013-08-19 20:32:17 +0200

    avinor: Use moment.js for all date formatting

--- app/static/widgets/avinor/avinor.js
+++ app/static/widgets/avinor/avinor.js
@@ -5,23 +5,13 @@ jarvis.controller('AvinorCtrl', ['$scope',
   function ($scope) {
     'use strict';
 
-    var getMonthName = function (month) {
-      return ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli',
-        'august', 'september', 'oktober', 'november',
-        'desember'][month - 1];
-    };
-
     $scope.$on('avinor', function (ev, body) {
-      body.flights.map(function (d) {
-        var date = moment(d.schedule_time);
-        d.day = date.format('D');
-        d.month = getMonthName(date.format('M'));
-        d.monthShort = d.month.substring(0, 3);
-        d.time = date.format('HH:mm');
+      body.flights.map(function (flight) {
+        flight.date = moment(flight.schedule_time);
       });
       if (body.flights.length > 0) {
         body.next = body.flights[0];
-        body.flights = body.flights.slice(1, 6);
+        body.flights = body.flights.slice(1, 5);
       } else {
         body.next = null;
         body.flights = [];
