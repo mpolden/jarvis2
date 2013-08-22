e149c7694dd62c06f47465306ae7ddfac47cde0f
Martin Polden <martin.polden@gmail.com>
2013-08-22 18:19:42 +0200

    Use border-sizing: border-box for sane box model

--- app/static/js/app/gridster.js
+++ app/static/js/app/gridster.js
@@ -2,7 +2,7 @@
 $(function () {
   'use strict';
   $('.gridster ul').gridster({
-    widget_margins: [5, 10],
-    widget_base_dimensions: [140, 140]
+    widget_margins: [5, 5],
+    widget_base_dimensions: [145, 145]
   });
 });
