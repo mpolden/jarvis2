02a9390cd8d94afc53bf7b8d281f76bcbc5c7ef7
Martin Polden <martin.polden@gmail.com>
2013-09-22 14:54:12 +0200

    gruntfile: Remove unused uglify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,6 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-jsbeautifier');
 
