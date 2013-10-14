3d3120a188eb49aca04736c856600a276a3259d4
Martin Polden <martin.polden@gmail.com>
2013-10-14 17:56:36 +0200

    calendar: Ensure that today is null or object
    The today property should be null if there is no event scheduled for
    today. This prevents todays most recent event, if any, still being visible in
    the view.

--- app/static/widgets/calendar/calendar.js
+++ app/static/widgets/calendar/calendar.js
@@ -5,19 +5,20 @@ jarvis.controller('CalendarCtrl', ['$scope',
     'use strict';
 
     $scope.$on('calendar', function (ev, body) {
+      if (body.events.length === 0) {
+        return;
+      }
       body.events.forEach(function (e) {
         e.date = moment(e.date).lang('nb');
       });
-      if (body.events.length > 0) {
-        var eventDate = body.events[0].date,
-          now = moment();
-        if (eventDate.isBefore(now) || eventDate.isSame(now, 'day')) {
-          body.today = body.events[0];
-          body.events = body.events.slice(1, 5);
-        } else {
-          body.events = body.events.slice(0, 4);
-        }
+      var eventDate = body.events[0].date,
+        now = moment();
+      if (eventDate.isBefore(now) || eventDate.isSame(now, 'day')) {
+        body.today = body.events.shift() || null;
+      } else {
+        body.today = null;
       }
+      body.events = body.events.slice(0, 4);
       angular.extend($scope, body);
     });
 
