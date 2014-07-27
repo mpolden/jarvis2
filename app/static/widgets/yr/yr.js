1956af62d466ec6709eb28355ea572e9dc170fb4
Martin Polden <martin.polden@gmail.com>
2014-07-27 23:57:50 +0200

    Convert yr widget to Mithril

--- app/static/widgets/yr/yr.js
+++ app/static/widgets/yr/yr.js
@@ -1,12 +1,34 @@
-var jarvis = jarvis || angular.module('jarvis', []);
+var yr = {
+  'el': document.getElementById('yr')
+};
 
-jarvis.controller('YrCtrl', ['$scope',
-  function ($scope) {
-    'use strict';
+yr.controller = function () {
+  var ctrl = this;
+  ctrl.data = {};
+  yr.el.addEventListener('yr', function (event) {
+    ctrl.data = event.detail;
+    m.render(yr.el, yr.view(ctrl));
+  });
+};
 
-    $scope.$on('yr', function (ev, body) {
-      angular.extend($scope, body);
-    });
-
+yr.view = function (ctrl) {
+  if (Object.keys(ctrl.data).length === 0) {
+    return m('p', 'Waiting for data');
   }
-]);
+  return m('div', [
+    m('p.fade', 'Været i ' + ctrl.data.today.location),
+    m('h1', ctrl.data.today.temperature + '°'),
+    m('p', ctrl.data.today.description),
+    m('p.wind', ctrl.data.today.wind.description + ' (' +
+      ctrl.data.today.wind.speed + ' m/s) fra ' +
+      ctrl.data.today.wind.direction.toLowerCase()),
+    m('p.tomorrow', 'I morgen: ' + ctrl.data.tomorrow.temperature +
+      '° (' + ctrl.data.tomorrow.description.toLowerCase() + ')'),
+    m('p', {'class': 'fade updated-at'}, 'Sist oppdatert: ' +
+      ctrl.data.updatedAt)
+  ]);
+};
+
+if (yr.el !== null) {
+  m.module(yr.el, yr);
+}
