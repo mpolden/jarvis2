4476965c59a8b8b30717c3b14111128f11e62133
Martin Polden <martin.polden@gmail.com>
2014-07-28 15:19:17 +0200

    Convert sonos widget to Mithril

--- app/static/widgets/sonos/sonos.js
+++ app/static/widgets/sonos/sonos.js
@@ -1,26 +1,61 @@
-var jarvis = jarvis || angular.module('jarvis', []);
+var sonos = {
+  'el': document.getElementById('sonos')
+};
 
-jarvis.controller('SonosCtrl', ['$scope',
-  function ($scope) {
-    'use strict';
-
-    var stateName = function (state) {
-      if (state === 'PAUSED_PLAYBACK') {
-        return 'Pause';
-      }
-      if (state === 'PLAYING') {
-        return 'Spiller av';
-      }
-      if (state === 'STOPPED') {
-        return 'Stoppet';
-      }
-      return '';
-    };
-
-    $scope.$on('sonos', function (ev, body) {
-      body.state = stateName(body.state);
-      angular.extend($scope, body);
-    });
-
+sonos.stateName = function (state) {
+  if (state === 'PAUSED_PLAYBACK') {
+    return 'Pause:';
   }
-]);
+  if (state === 'PLAYING') {
+    return 'Spiller av:';
+  }
+  if (state === 'STOPPED') {
+    return 'Stoppet';
+  }
+  return '';
+};
+
+sonos.controller = function () {
+  var ctrl = this;
+  ctrl.data = {};
+  sonos.el.addEventListener('sonos', function (event) {
+    var body = event.detail;
+    body.state = sonos.stateName(body.state);
+    ctrl.data = body;
+    m.render(sonos.el, sonos.view(ctrl));
+  });
+};
+
+sonos.view = function (c) {
+  if (Object.keys(c.data).length === 0) {
+    return m('p', 'Waiting for data');
+  }
+  var current = [];
+  if (c.data.current) {
+    current = [
+      m('h1', jrvs.truncate(c.data.current.artist, 14) + ' - ' +
+        jrvs.truncate(c.data.current.title, 16)),
+      m('p', {'class': 'fade duration'}, c.data.current.position + ' / ' +
+        c.data.current.duration)
+    ];
+  }
+  var next = [];
+  if (c.data.next) {
+    next = [
+      m('p', {'class': 'fade next'}, 'Neste i kø:'),
+      m('p', jrvs.truncate(c.data.next.artist, 15) + ' - ' +
+        jrvs.truncate(c.data.next.title, 20))
+    ];
+  }
+  return [
+    m('p.fade', c.data.state),
+    m('div', current),
+    m('div', next),
+    m('p', {'class': 'fade updated-at'}, 'Sist oppdatert: ' +
+      c.data.updatedAt)
+  ];
+};
+
+if (sonos.el !== null) {
+  m.module(sonos.el, sonos);
+}
