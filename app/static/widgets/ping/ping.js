8e4698e8933d109ed6af8fa5fde99ac21817166e
Martin Polden <martin.polden@gmail.com>
2014-07-28 14:02:24 +0200

    Convert ping widget to Mithril

--- app/static/widgets/ping/ping.js
+++ app/static/widgets/ping/ping.js
@@ -1,62 +1,73 @@
 /* jshint nonew: false */
 
-var jarvis = jarvis || angular.module('jarvis', []);
+var ping = {
+  'el': document.getElementById('ping')
+};
 
-jarvis.controller('PingCtrl', ['$scope',
-  function ($scope) {
-    'use strict';
+ping.controller = function () {
+  var ctrl = this;
+  ctrl.graph = null;
+  ping.el.addEventListener('ping', function (event) {
+    ctrl.data = event.detail;
+    m.render(ping.el, ping.view(ctrl));
+    if (ctrl.graph === null) {
+      ctrl.graph = ping.createGraph(ping.el, ctrl.data.values);
+    } else {
+      ctrl.graph.series.addData(ctrl.data.values);
+    }
+    ctrl.graph.render();
+  });
+};
 
-    var graph = null;
+ping.view = function () {
+  return [
+    m('div#y-axis'),
+    m('div#chart'),
+    m('div#legend')
+  ];
+};
 
-    var createGraph = function (element, values) {
-      var labels = Object.keys(values).map(function (name) {
-        return {
-          name: name
-        };
-      });
-      var series = new Rickshaw.Series.FixedDuration(
-        labels,
-        new Rickshaw.Color.Palette({
-          scheme: 'colorwheel'
-        }), {
-          timeInterval: 5000,
-          maxDataPoints: 100,
-          timeBase: new Date().getTime() / 1000
-        }
-      );
-      var _graph = new Rickshaw.Graph({
-        element: element,
-        width: 535,
-        height: 240,
-        renderer: 'line',
-        stroke: true,
-        unstack: true,
-        series: series
-      });
-      new Rickshaw.Graph.Legend({
-        element: document.querySelector('#legend'),
-        graph: _graph
-      });
-      new Rickshaw.Graph.Axis.Y({
-        element: document.querySelector('#y-axis'),
-        graph: _graph,
-        orientation: 'left',
-        ticks: 5,
-        tickFormat: function (n) {
-          return n + ' ms';
-        }
-      });
-      return _graph;
+ping.createGraph = function (element, values) {
+  var labels = Object.keys(values).map(function (name) {
+    return {
+      name: name
     };
+  });
+  var series = new Rickshaw.Series.FixedDuration(
+    labels,
+    new Rickshaw.Color.Palette({
+      scheme: 'colorwheel'
+    }), {
+      timeInterval: 5000,
+      maxDataPoints: 100,
+      timeBase: new Date().getTime() / 1000
+    }
+  );
+  var graph = new Rickshaw.Graph({
+    element: element,
+    width: 535,
+    height: 240,
+    renderer: 'line',
+    stroke: true,
+    unstack: true,
+    series: series
+  });
+  var _ = new Rickshaw.Graph.Legend({
+    element: document.querySelector('#legend'),
+    graph: graph
+  });
+  var _ = new Rickshaw.Graph.Axis.Y({
+    element: document.querySelector('#y-axis'),
+    graph: graph,
+    orientation: 'left',
+    ticks: 5,
+    tickFormat: function (n) {
+      return n + ' ms';
+    }
+  });
+  return graph;
+};
 
-    $scope.$on('ping', function (ev, body) {
-      if (graph === null) {
-        var element = document.querySelector('#ping #chart');
-        graph = createGraph(element, body.values);
-      } else {
-        graph.series.addData(body.values);
-      }
-      graph.render();
-    });
-  }
-]);
+if (ping.el !== null) {
+  m.module(ping.el, ping);
+}
