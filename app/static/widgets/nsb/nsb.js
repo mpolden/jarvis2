b529b656dd73a2cd85b2cfd24dd295af46c9b4d7
Martin Polden <martin.polden@gmail.com>
2014-07-28 03:47:03 +0200

    Convert nsb widget to Mithril

--- app/static/widgets/nsb/nsb.js
+++ app/static/widgets/nsb/nsb.js
@@ -1,19 +1,53 @@
-var jarvis = jarvis || angular.module('jarvis', []);
+var nsb = {
+  'el': document.getElementById('nsb')
+};
 
-jarvis.controller('NsbCtrl', ['$scope',
-  function ($scope) {
-    'use strict';
+nsb.controller = function () {
+  var ctrl = this;
+  ctrl.data = {};
+  nsb.el.addEventListener('nsb', function (event) {
+    var body = event.detail;
+    if (body.departures.length > 0) {
+      body.next = body.departures[0];
+      body.upcoming = body.departures.slice(1, 5);
+    } else {
+      body.next = null;
+      body.upcoming = [];
+    }
+    ctrl.data = body;
+    m.render(nsb.el, nsb.view(ctrl));
+  });
+};
 
-    $scope.$on('nsb', function (ev, body) {
-      if (body.departures.length > 0) {
-        body.next = body.departures[0];
-        body.upcoming = body.departures.slice(1, 5);
-      } else {
-        body.next = null;
-        body.upcoming = [];
-      }
-      angular.extend($scope, body);
-    });
-
+nsb.view = function (c) {
+  if (Object.keys(c.data).length === 0) {
+    return m('p', 'Waiting for data');
   }
-]);
+  var rows = c.data.upcoming.map(function (departure) {
+    return m('tr', [
+      m('td', departure.departure),
+      m('td', departure.arrival),
+      m('td', departure.duration + ' min')
+    ]);
+  });
+  return m('div', [
+    m('p.fade', 'Neste tog fra ' + c.data.from + ' til ' + c.data.to +
+      ' går ' + c.data.date),
+    m('h1', c.data.next.departure),
+    m('h2.fade', 'Ankomst: ' + c.data.next.arrival + ' (' +
+      c.data.next.duration + ' min)'),
+    m('table', [
+      m('tr.fade', [
+        m('th', 'Avgang'),
+        m('th', 'Ankomst'),
+        m('th', 'Reisetid')
+      ])
+    ].concat(rows)),
+    m('p', {class: 'fade updated-at'}, 'Sist oppdatert: ' +
+      c.data.updatedAt)
+  ]);
+};
+
+if (nsb.el !== null) {
+  m.module(nsb.el, nsb);
+}
