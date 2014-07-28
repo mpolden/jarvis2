84e064e9d8932d3819b34c81de495a509e14acbc
Martin Polden <martin.polden@gmail.com>
2014-07-28 14:38:10 +0200

    Convert uptime widget to Mithril

--- app/static/widgets/uptime/uptime.js
+++ app/static/widgets/uptime/uptime.js
@@ -1,15 +1,38 @@
-var jarvis = jarvis || angular.module('jarvis', []);
+var uptime = {
+  'el': document.getElementById('uptime')
+};
 
-jarvis.controller('UptimeCtrl', ['$scope',
-  function ($scope) {
-    'use strict';
-
-    $scope.$on('uptime', function (ev, body) {
-      body.hosts.forEach(function (host) {
-        host.active = host.active ? 'aktiv' : 'inaktiv';
-      });
-      angular.extend($scope, body);
+uptime.controller = function () {
+  var ctrl = this;
+  ctrl.data = {};
+  uptime.el.addEventListener('uptime', function (event) {
+    var body = event.detail;
+    body.hosts.forEach(function (host) {
+      host.active = host.active ? 'aktiv' : 'inaktiv';
     });
+    ctrl.data = body;
+    m.render(uptime.el, uptime.view(ctrl));
+  });
+};
 
+uptime.view = function (c) {
+  if (Object.keys(c.data).length === 0) {
+    return m('p', 'Waiting for data');
   }
-]);
+  var rows = c.data.hosts.map(function (host) {
+    return m('tr', [
+      m('td.label', host.label),
+      m('td.active', host.active)
+    ]);
+  });
+  return [
+    m('p.fade', 'Enheter'),
+    m('table', rows),
+    m('p', {'class': 'fade updated-at'}, 'Sist oppdatert: ' +
+      c.data.updatedAt)
+  ];
+};
+
+if (uptime.el !== null) {
+  m.module(uptime.el, uptime);
+}
