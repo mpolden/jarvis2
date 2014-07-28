93545f180bc380c8e808c46cc23ed5103279ccaf
Martin Polden <martin.polden@gmail.com>
2014-07-28 03:59:29 +0200

    Convert hackernews widget to Mithril

--- app/static/widgets/hackernews/hackernews.js
+++ app/static/widgets/hackernews/hackernews.js
@@ -1,15 +1,37 @@
-var jarvis = jarvis || angular.module('jarvis', []);
+var hn = {
+  'el': document.getElementById('hackernews')
+};
 
-jarvis.controller('HackernewsCtrl', ['$scope',
-  function ($scope) {
-    'use strict';
+hn.controller = function () {
+  this.data = {};
+  hn.el.addEventListener('hackernews', function (event) {
+    var body = event.detail;
+    if (body.items && body.items.length > 0) {
+      body.items = body.items.slice(0, 10);
+    }
+    this.data = body;
+    m.render(hn.el, hn.view(this));
+  });
+};
 
-    $scope.$on('hackernews', function (ev, body) {
-      if (body.items && body.items.length > 0) {
-        body.items = body.items.slice(0, 10);
-      }
-      angular.extend($scope, body);
-    });
-
+hn.view = function (c) {
+  if (Object.keys(c.data).length === 0) {
+    return m('p', 'Waiting for data');
   }
-]);
+  var rows = c.data.items.map(function (item) {
+    return m('tr', [
+      m('td.title', jrvs.truncate(item.title, 24)),
+      m('td.points', item.points)
+    ]);
+  });
+  return m('div', [
+    m('p.fade', 'Hacker News'),
+    m('table', rows),
+    m('p', {class: 'fade updated-at'}, 'Sist oppdatert: ' +
+      c.data.updatedAt)
+  ]);
+};
+
+if (hn.el !== null) {
+  m.module(hn.el, hn);
+}
