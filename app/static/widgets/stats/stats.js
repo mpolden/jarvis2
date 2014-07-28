33e9973f250d8ed977732f90461b8ae82a85288e
Martin Polden <martin.polden@gmail.com>
2014-07-28 15:40:41 +0200

    Convert stats widget to Mithril

--- app/static/widgets/stats/stats.js
+++ app/static/widgets/stats/stats.js
@@ -1,41 +1,47 @@
-var jarvis = jarvis || angular.module('jarvis', []);
+var stats = {
+  'el': document.getElementById('stats')
+};
 
-jarvis.controller('StatsCtrl', ['$scope',
-  function ($scope) {
-    'use strict';
+stats.controller = function () {
+  var ctrl = this;
+  ctrl.data = {};
+  stats.el.addEventListener('stats', function (event) {
+    ctrl.data = event.detail;
+    m.render(stats.el, stats.view(ctrl));
+  });
+};
 
-    var gauges = {
-      coffee: null,
-      beer: null
-    };
-    var opts = {
-      lines: 8,
-      angle: 0.15,
-      lineWidth: 0.3,
-      pointer: {
-        length: 0.85,
-        strokeWidth: 0.045,
-        color: '#ffffff'
-      },
-      limitMax: false,
-      strokeColor: '#665847'
-    };
-
-    var updateGauge = function (gauge, body) {
-      if (gauges[gauge] === null) {
-        var target = document.querySelector('#stats #' + gauge);
-        var textField = document.querySelector('#stats #' + gauge + '-count');
-        gauges[gauge] = new Gauge(target).setOptions(opts);
-        gauges[gauge].setTextField(textField);
-      }
-      gauges[gauge].maxValue = body.max[gauge];
-      gauges[gauge].set(body.stats[gauge]);
-    };
-
-    $scope.$on('stats', function (ev, body) {
-      updateGauge('coffee', body);
-      updateGauge('beer', body);
-      angular.extend($scope, body);
-    });
+stats.view = function (c) {
+  if (Object.keys(c.data).length === 0) {
+    return m('p', 'Waiting for data');
   }
-]);
+  return [
+    m('h1', 'Dagens forbruk'),
+    m('table', [
+      m('tr.fade', [
+        m('th', 'Drikke'),
+        m('th', 'Antall')
+      ]),
+      m('tr', [
+        m('td', 'Kaffe'),
+        m('td', [
+          m('span', c.data.stats.coffee),
+          m('span.fade', ' / ' + c.data.max.coffee)
+        ])
+      ]),
+      m('tr', [
+        m('td', 'Øl'),
+        m('td', [
+          m('span', c.data.stats.beer),
+          m('span.fade', ' / ' + c.data.max.beer)
+        ])
+      ])
+    ]),
+    m('p', {'class': 'fade updated-at'}, 'Sist oppdatert: ' +
+      c.data.updatedAt)
+  ];
+};
+
+if (stats.el !== null) {
+  m.module(stats.el, stats);
+}
