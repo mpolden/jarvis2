d8b666497aea0e98bc89858942b5aebe70706022
Martin Polden <mpolden@mpolden.no>
2017-09-16 13:34:11 +0200

    Make ping job work on darwin

--- app/jobs/ping.py
+++ app/jobs/ping.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from collections import deque
 from jobs import AbstractJob
 from subprocess import Popen, PIPE
+from sys import platform
 
 
 def _items(d):
@@ -28,9 +29,14 @@ class Ping(AbstractJob):
         time = re.search(r'time=(\d+(?:\.\d+)?)', ping_output)
         return float(time.group(1)) if time is not None else 0
 
-    def _get_latency(self, host):
+    def _ping(self, host):
         ping_cmd = 'ping6' if ':' in host else 'ping'
-        ping = '%s -w %d -c 1 %s' % (ping_cmd, self.timeout, host)
+        deadline_flag = '-w'
+        # ping on darwin uses -t for deadline/timeout
+        if platform == 'darwin':
+            deadline_flag = '-t'
+        ping = '%s %s %d -c 1 %s' % (ping_cmd, deadline_flag, self.timeout,
+                                     host)
         p = Popen(ping.split(' '), stdout=PIPE, stderr=PIPE)
         return self._parse_time(p.communicate()[0].decode())
 
@@ -39,7 +45,7 @@ class Ping(AbstractJob):
         for label, host in self.hosts:
             if label not in self.values:
                 self.values[label] = deque([], self.cache_size)
-            latency = self._get_latency(host)
+            latency = self._ping(host)
             self.values[label].append({
                 'label': label,
                 'host': host,
