03b5aebba8ca7e633be11ec64fdb8bf3811c635b
Martin Polden <mpolden@mpolden.no>
2017-11-20 19:30:36 +0100

    rss: Display age of each item

--- jarvis/static/widgets/rss/rss.js
+++ jarvis/static/widgets/rss/rss.js
@@ -1,12 +1,22 @@
 var rss = rss || {};
 
+rss.parse = function (data) {
+  var items = data.items.slice(0, 6).map(function (item) {
+    return {'title': item.title, 'time': moment.unix(item.time).locale('nb')};
+  });
+  data.items = items;
+  return data;
+};
+
 rss.view = function (vnode) {
   if (Object.keys(vnode.attrs.data).length === 0) {
     return m('p', 'Waiting for data');
   }
-  var state = vnode.attrs.data;
-  var items = state.items.slice(0, 6).map(function (item) {
-    return m('li', jrvs.truncate(item.title, 60));
+  var state = rss.parse(vnode.attrs.data);
+  var items = state.items.map(function (item) {
+    return m('li', jrvs.truncate(item.title, 45),
+             m('span', {class: 'fade age'}, ' — ' + item.time.fromNow(true))
+            );
   });
   return [
     m('p.fade', state.title),
